9a32fcf2be14e4ae4e1778ee9d4f8245dcdd9ddf
TRAINEDTURTLE <cjxz2006@gmail.com>
2022-06-12 14:37:57 +0800

    update

---
+++ screens/EventsScreen.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+const Stack = createStackNavigator()
+
+function EventsScreen ({navigation}){
+  const Navigation = useNavigation()
+  return (<View style={{flex:1,justifyContent:'center',aligntItems:'center',backgroundcolor:'lightgreen'}}>
+    <Text>Events First Page!</Text>
+    <Button onPress={()=>navigation.navigate("EventsSecond")} title="Second Screen"/>
+  </View>
+  )
+}
+
+function EventsSecondScreen(){
+  return (
+    <View style={{flex:1,justifyContent:'center',aligntItems:'center',backgroundcolor:'lightgreen'}}>
+      <Text>Events Second Page!</Text>
+    </View>
+  )
+}
+export default function EventsStack(){
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="EventsHome" component={EventsScreen}/>
+        <Stack.Screen name="EventsSecond" component={EventsSecondScreen}/>
+      </Stack.Navigator>
+    );
+  }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View, FlatList } from 'react-native';
+import CalendarStack from '../components/calendar'
+//import progress from '../components/progress'
+
+export default function HomeScreen() {
+    return (
+      <CalendarStack/>
+    );
+  }
