42c44233c7baea2c57d6b780299f198a6482d342
Glenda Yap <glendaykh@gmail.com>
2022-06-13 14:17:23 +0800

    Changed Forum

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -1,10 +1,53 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { createStackNavigation } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
 
-export default function ForumScreen() {
+function ForumScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Input discussion!</Text>
+      <View style={{ flex: 1, justifyContent: 'top', alignItems: 'left' }}>
+        <Text>Input discussion here!</Text>
+        <Button 
+          title = "Saved"
+          onPress = {() => {
+            navigation.navigate('ForumSecondScreen')
+          }}
+        />
       </View>
     );
+  }
+
+  function ForumSecondScreen() {
+    return (
+      <View style = {{ flex: 1, justifyContent: 'top', alignItems: 'center' }} > 
+        <Text>Saved items</Text>
+        <Button 
+          title = "Popular"
+          onPress = {() => {
+            navigation.navigate('ForumThirdScreen')
+          }}
+        /> 
+      </View>
+    )
+  }
+
+  function ForumThirdScreen() {
+    return (
+      <View 
+        style = {{ flex: 1, justifyContent: 'top', alignItems: 'center' }} > 
+        <Text>Following items</Text>   
+      </View>
+    )
+  }
+
+  const Stack  =createStackNavigation();
+
+  export default function ForumStack() {
+    return (
+      <Stack.Navigator> 
+        <Stack.Screen name = "Popular" component = {ForumScreen} />
+        <Stack.Screen name = "Saved" component = {ForumSecondScreen} />
+        <Stack.Screen name = "Following" component = {ForumThirdScreen} />
+      </Stack.Navigator>
+    )
   }
