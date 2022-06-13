4fa77f124097625ff4631cb55f2f9bb0a4f18840
Glenda Yap <glendaykh@gmail.com>
2022-06-13 15:50:07 +0800

    removed tabs

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { Text, View, Button, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
-import { TabView, SceneMap } from 'react-native-tab-view';
+
 
 function ForumScreen() {
   const navigation = useNavigation();
@@ -55,52 +55,3 @@ function ForumScreen() {
       </Stack.Navigator>
     )
   }
-
-  const FirstRoute = () => (
-    <View style={[styles.scene, { backgroundColor: 'white' }]} />
-  );
-  
-  const SecondRoute = () => (
-    <View style={[styles.scene, { backgroundColor: 'white' }]} />
-  );
-  
-  const ThirdRoute = () => (
-    <View style={[styles.scene, { backgroundColor: 'white' }]} />
-  );
-  
-  
-  const initialLayout = { width: Dimensions.get('window').width };
-  
-  const renderScene = SceneMap({
-    first: FirstRoute,
-    second: SecondRoute,
-    third: ThirdRoute,
-  });
-  
-  export default function TabViewExample() {
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-      { key: 'first', title: 'Popular' },
-      { key: 'second', title: 'Saved' },
-      { key: 'third', title: 'Following'}
-    ]);
-  
-    return (
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={initialLayout}
-        style={styles.container}
-      />
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      marginTop: StatusBar.currentHeight,
-    },
-    scene: {
-      flex: 1,
-    },
-  });
