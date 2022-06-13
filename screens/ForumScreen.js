0ae80c338517aabe30f4939c06a22eea4d1ab85b
Glenda Yap <glendaykh@gmail.com>
2022-06-13 15:33:15 +0800

    added tabs

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -55,3 +55,56 @@ function ForumScreen() {
       </Stack.Navigator>
     )
   }
+
+  import * as React from 'react';
+import { View, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: 'white' }]} />
+);
+
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: 'white' }]} />
+);
+
+const ThirdRoute = () => (
+  <View style={[styles.scene, { backgroundColor: 'white' }]} />
+);
+
+
+const initialLayout = { width: Dimensions.get('window').width };
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+});
+
+export default function TabViewExample() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Popular' },
+    { key: 'second', title: 'Saved' },
+    { key: 'third', title: 'Following'}
+  ]);
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      style={styles.container}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+  },
+  scene: {
+    flex: 1,
+  },
+});
