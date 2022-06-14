8283967fc4a5d1512d5936aa8888fdd0619c2afc
Glenda Yap <glendaykh@gmail.com>
2022-06-14 23:04:44 +0800

    erased search bar

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -8,28 +8,6 @@
 import React,{ Component as ForumTabs } from 'react';
 import {StyleSheet, View, Dimensions} from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
-import { useState, useEffect } from 'react';
-import {
-    SafeAreaView,
-    ScrollView,
-    Text,
-    Image,
-    StatusBar,
-    FlatList,
-    TouchableOpacity,
-    TextInput
-} from 'react-native';
-import Searchbar from '../components/SearchBar'
-
-const styles = StyleSheet.create({
-  scene: {
-      flex: 1
-  },
-  container: {
-    flex: 1,
-    // backgroundColor: 'red', height: '100%', width: '100%' 
-    },
-});
 
 export default class App extends ForumTabs {
     constructor(props){
@@ -47,7 +25,7 @@ export default class App extends ForumTabs {
         switch(route.key){
             case 'Popular':
                 return <View style = {[styles.scene, {backgroundColor: 'white' }]} />;
-            case 'Savedr':
+            case 'Saved':
                 return <View style = {[styles.scene, {backgroundColor: 'white' }]} />;
             case 'Following':
                 return <View style = {[styles.scene, {backgroundColor: 'white' }]} />;
@@ -57,34 +35,17 @@ export default class App extends ForumTabs {
     render() {
         return(
             <TabView
-                navigationState={{index: this.state.index , routes: this.state.route}}
+                navigationState={{index: this.state.index , routes: this.state.routes}}
                 renderScene={this.renderScene}
                 onIndexChange={index=>this.setState({index})}
                 initialLayout={{width:Dimensions.get('window').width}}
             />
         );
     }
-    constructor() {
-        const [value, setValue] = useState();
-        function updateSearch(value) {
-            //do your search logic or anything
-            console.log(value);
-        }
-        return (
-
-            <View style={styles.container}>
-                <View style={{ height: '20%', backgroundColor: "#3F569C", borderRadius: 10, }}>
-                    <Image
-                        style={{ marginTop: '15%', marginLeft: '5%' }}
-                        source={require('../assets/ic_back.png')} />
-                    <Searchbar
-                        value={value}
-                        updateSearch={updateSearch}
-                        style={{ marginTop: '8%' }} />
-                </View>
-
-            </View>
-        );
-    }
-    render;
 }
+
+const styles = StyleSheet.create({
+    scene: {
+        flex: 1
+    },
+  });
